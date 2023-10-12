726e00d3e20c94feb3382287fb45d9d675660d4e
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-12 13:30:00 -0300

    feat [moments]: imported multer

--- routes/moments.js
+++ routes/moments.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const momentController = require("../controllers/momentController");
+const upload = require("../config/multer");
 
 router.route("/moments").post((req, res) => momentController.create(req, res));
 
