008c3e19b5808893dd43b6463d932e991594f574
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-13 19:12:52 -0300

    feat [pictureController]: created image controller

---
+++ controllers/pictureController.js
@@ -0,0 +1,25 @@
+const { Picture } = require("../models/Picture");
+
+const pictureController = {
+  create: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      const file = req.file;
+
+      const picture = new Picture({
+        name,
+        src: file.path,
+      });
+
+      await picture.save();
+
+      res.status(200).json({ picture, message: "Imagem enviada!" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Falha ao enviar imagem." });
+    }
+  },
+};
+
+module.exports = pictureController;
