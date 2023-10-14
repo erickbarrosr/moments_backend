de0e96abe041c188bc03af86edc890562fe4b4b4
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-13 21:24:32 -0300

    refactor [router]: disable picture router

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const momentsRouter = require("./moments");
-const picturesRouter = require("./pictures");
+//const picturesRouter = require("./pictures");
 
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Bem vindo ao Moments!" });
 });
 
 router.use("/", momentsRouter);
-router.use("/", picturesRouter);
+//router.use("/", picturesRouter);
 
 module.exports = router;
