9b89718f7187df4c22bac1362fc9c034cc495fd1
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-14 13:41:55 -0300

    feat [momentController]: add methods update and destroy

--- controllers/momentController.js
+++ controllers/momentController.js
@@ -51,7 +51,7 @@ const momentController = {
       res.status(500).json({ message: "Erro interno de servidor!" });
     }
   },
-  index: async (req, res) => {
+  index: async (res) => {
     try {
       const moments = await Moment.find();
 
@@ -80,48 +80,58 @@ const momentController = {
   update: async (req, res) => {
     try {
       const id = req.params.id;
+      const { title, author, description, image } = req.body;
 
-      const { title, author, description } = req.body;
-
-      if (!title) {
-        return res.status(422).json({ message: "Título obrigatório." });
-      }
-
-      if (!author) {
-        return res.status(422).json({ message: "Autor obrigatório." });
-      }
-
-      if (!description) {
-        return res.status(422).json({ message: "Descrição obrigatória." });
-      }
-
-      if (!req.file) {
-        return res
-          .status(422)
-          .json({ message: "Faça o upload de uma imagem." });
-      }
-
-      const newPicture = new Picture({
-        name: req.file.originalname,
-        src: req.file.path,
-      });
-
-      const savedPicture = await newPicture.save();
-
-      const moment = {
+      const momentUpdateData = {
         title,
         author,
         description,
-        image: savedPicture._id,
+        image,
       };
 
-      const updatedMoment = await Moment.findByIdAndUpdate(id, moment);
+      // Verifica se a solicitação contém um arquivo de imagem
+      if (req.file) {
+        const newPicture = new Picture({
+          name: req.file.originalname,
+          src: req.file.path,
+        });
+
+        const savedPicture = await newPicture.save();
+
+        // Atualiza o momento com o ID da nova imagem
+        momentUpdateData.image = savedPicture._id;
+      }
+
+      const updatedMoment = await Moment.findByIdAndUpdate(
+        id,
+        momentUpdateData,
+        // Faz com que a função retorne o objeto atualizado em vez do objeto anterior à atualização
+        {
+          new: true,
+        }
+      );
 
       if (!updatedMoment) {
         return res.status(404).json({ message: "Momento não encontrado." });
       }
 
       res.status(200).json({ updatedMoment, message: "Momento atualizado!" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Erro interno de servidor!" });
+    }
+  },
+  destroy: async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const deletedMoment = await Moment.findByIdAndDelete(id);
+
+      if (!deletedMoment) {
+        return res.status(404).json({ message: "Momento não encontrado." });
+      }
+
+      res.status(200).json({ deletedMoment, message: "Momento excluído!" });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Erro interno de servidor!" });
