45d5609f2835b36bedf379a717bbf2093c9785d9
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-15 12:55:38 -0300

    feat [router]: imported comments router

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const momentsRouter = require("./moments");
+const commentsRouter = require("./comments");
 //const picturesRouter = require("./pictures");
 
-router.route("/").get((res) => {
+router.route("/").post((res) => {
   res.status(200).json({ message: "Bem vindo ao Moments!" });
 });
 
 router.use("/", momentsRouter);
+router.use("/", commentsRouter);
 //router.use("/", picturesRouter);
 
 module.exports = router;
